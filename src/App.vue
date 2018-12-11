06b3178a9f0e53e75bd110fb614f6c573981d226
Roils <roshall@yeah.net>
2018-12-11 11:07:54 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <app-header v-bind:title="title">111</app-header>
     <app-users v-bind:users="users">2222</app-users>
     <app-users v-bind:users="users">3333</app-users>
-    <app-footer v-bind:title="title">44444</app-footer>
+    <app-footer v-bind:title="title">666666</app-footer>
   </div>
 </template>
 
