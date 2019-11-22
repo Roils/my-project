e03c48a4bcaec48baf7f9f61d4005dc2ef413783
“Roils <roshall@yeah.net>
2019-11-22 23:49:40 +0800

    基本项目配置

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,16 @@
 <template>
   <div id="app">
-    <app-header v-bind:title="title">111</app-header>
-    <app-users v-bind:users="users">2222</app-users>
-    <app-users v-bind:users="users">3333</app-users>
-    <app-footer v-bind:title="title">666666</app-footer>
+    12
   </div>
 </template>
 
 <script>
-import Header from './components/Header'
-import Footer from './components/Footer'
-import Users from './components/Users'
 export default {
   name: 'App',
   components: {
-    'app-header': Header,
-    'app-footer': Footer,
-    'app-users': Users
   },
   data () {
     return {
-      users: [
-        {name: 'Roil', position: 'Roil', show: false},
-        {name: 'Roil', position: 'Roil', show: false},
-        {name: 'Roil', position: 'Roil', show: false},
-        {name: 'Roil', position: 'Roil', show: false},
-        {name: 'Roil', position: 'Roil', show: false},
-        {name: 'Roil', position: 'Roil', show: false},
-        {name: 'Roil', position: 'Roil', show: false}
-      ]
     }
   }
 }
--- src/main.js
+++ src/main.js
@@ -3,8 +3,11 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+import "./assets/common/rem"
+import "./assets/css/reset.css"
 
 Vue.config.productionTip = false
+
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
