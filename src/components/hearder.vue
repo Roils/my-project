5bc9e832a0b77a36e50e95ba9e87403bfa956479
“Roils <roshall@yeah.net>
2019-11-23 00:08:26 +0800

    头部组件

---
+++ src/components/hearder.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+    12
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  components: {
+  },
+  data () {
+    return {
+    }
+  }
+}
+</script>
+
+<style scoped>
+</style>
+
